06a962492e77c9e50e8dc2ad14543995ae78eba5
Oparaugo Melody Ifeanyi <melodyleonard7@gmail.com>
2021-12-28 11:44:50 +0100

    added add allowed token function

--- contracts/TokenSwap.sol
+++ contracts/TokenSwap.sol
@@ -3,22 +3,25 @@
 pragma solidity ^0.8.4;
 pragma abicoder v2;
 
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
-import 'openzeppelin-solidity/contracts/access/Ownable.sol';
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 contract TokenSwap is Ownable{
 
     ISwapRouter public immutable swapRouter;
     uint256 public poolFee = 3000;
-    mapping (string => address) public allowedTokens;
+    mapping (string => address) public storage allowedTokens;
+
+    error InsufficientBalance(uint256 available, uint256 required);
 
     constructor(ISwapRouter _swapRouter) {
-        swapRouter = _swapRouter;
+        swapRouter = _swapRouter; 
     }
 
-    function addAllowedToken(string[] memory tokenName) public onlyOwner {
-        require(tokenName.length > 0, "You must provide a token to add");
+    mapping (string => address) public allowedTokens; 
+        function addAllowedToken(string memory _tokenName, address _token) public {
+        allowedTokens[_tokenName] = _token;
     }
 
     function modifyPoolFee(uint256 _poolFee) public onlyOwner{
@@ -27,10 +30,8 @@ contract TokenSwap is Ownable{
 
     function swapExactInputSingle(uint256 amountIn, string memory inputToken, string memory outputToken ) external returns (uint256 amountOut){
         IERC20(allowedTokens[inputToken]).approve(address(this), amountIn);
-
         // before this, give the owner of the contract his commission
         TransferHelper.safeApprove(allowedTokens[inputToken],  address(this), amountIn);
-
     }
 
 }
